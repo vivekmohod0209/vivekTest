2551bd207e610af4074713cbccd2b2433269ec91
Vivek Mohod <vivekmohod0209@gmail.com>
2023-02-06 16:32:29 +0530

    first commit

---
+++ src/main/java/com/example/practice/Sample.java
@@ -0,0 +1,12 @@
+package com.example.practice;
+
+public class Sample {
+public static void main(String[] args) {
+	int x = 100;
+	int a = x++;
+	int b = ++x;
+	int c = x++;
+	int d = (a<b)?(b<c)?a:(b<c)?b:c:b;
+	System.out.println(d);
+}
+}
