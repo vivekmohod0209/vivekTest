7c99dcd86a6ae7a79e8c88def10447ceb53144b9
Vivek Mohod <vivekmohod0209@gmail.com>
2023-02-09 22:46:50 +0530

    test

---
+++ src/main/java/com/example/practice/Test.java
@@ -0,0 +1,9 @@
+package com.example.practice;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("this is used to stash");
+	
+	System.out.println("this is to revert");
+}
+}
